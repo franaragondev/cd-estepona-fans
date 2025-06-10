a7f068b73b97576232b97b006fce0fbc6371291b
Fran Aragón <franaragondeveloper@gmail.com>
2025-06-10 18:50:16 +0200

    Add button to return to view all news

--- src/app/[locale]/noticias/[slug]/page.tsx
+++ src/app/[locale]/noticias/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { notFound } from "next/navigation";
+import Link from "next/link";
 import Image from "next/image";
 import { PrismaClient } from "@prisma/client";
 
@@ -45,6 +46,13 @@ export default async function Page({ params }: any) {
       <p className="text-lg text-gray-800 leading-relaxed mb-12">
         {article.content}
       </p>
+
+      <Link
+        href={`/${params.locale}/noticias`}
+        className="inline-block px-4 py-2 rounded text-white bg-[#DC2C20] hover:bg-[#2f36a1] transition-colors duration-200 cursor-pointer"
+      >
+        Volver a todas las noticias
+      </Link>
     </main>
   );
 }
