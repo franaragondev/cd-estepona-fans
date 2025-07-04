608a964c2e6a253eb47de940c73705dd9906725e
franaragondev <franaragondeveloper@gmail.com>
2025-07-04 12:20:11 +0200

    compress images

--- src/lib/optimizeImage.ts
+++ src/lib/optimizeImage.ts
@@ -1,5 +1,5 @@
 export function optimizeCloudinaryUrl(url: string): string {
   return url.includes("res.cloudinary.com")
-    ? url.replace("/upload/", "/upload/f_webp,q_auto/")
+    ? url.replace("/upload/", "/upload/f_webp,q_auto:eco/")
     : url;
 }
